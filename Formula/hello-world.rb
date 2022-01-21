d254d5346a58f5f9efe000f4e322cd6857cbf86c
Ryan Machado <rmachado.ctr@kr.af.mil>
2022-01-21 16:22:37 -0500

    init

---
+++ Formula/hello-world.rb
@@ -0,0 +1,23 @@
+class HelloWorld < Formula
+  desc "desc"
+  homepage "https://github.com/rymach/helloworld"
+  version "v9.0.0"
+
+  on_macos do
+    url "https://github.com/rymach/hello-world/archive/refs/tags/v9.0.0.zip"
+    sha256 "7bb080e2bd3928ae0cd275baede6e1fcc4a1e8252d15598254cda609cb0095f3"
+  end
+#   on_linux do
+#     url "https://github.com/int128/kubelogin/releases/download/v1.24.0/kubelogin_linux_amd64.zip"
+#     sha256 "658499b338b8cee7c622130d68405a55b4c4051ba43f7c841cb3f3d9364b3292"
+#   end
+
+  def install
+    system "make"
+    bin.install "build/hello"
+  end
+
+  test do
+    assert_equal `hello`, "Hello, #{ENV["USER"]}!\nVersion #{version}\n"
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Hello, World!
+
+## About
+
+This tap's only purpose it to create a working example showing how to build bottles for taps.
+
+## How do I install these formulae?
+
+`brew install rymach/hello-world/hello-world`
+
+Or `brew tap rymach/hello-world` and then `brew install hello-world`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
